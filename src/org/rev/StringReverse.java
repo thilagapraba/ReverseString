4dfeeff7e4e4fb96d94ae370f5b0926c7cb143e0
thilagapraba <thilagapraba8479@gmail.com>
2023-03-02 15:48:03 +0530

    Java pgm

---
+++ src/org/rev/StringReverse.java
@@ -0,0 +1,13 @@
+package org.rev;
+
+public class StringReverse {
+public static void main(String[] args) {
+	//String reverse
+	String s="thilagavathi";
+	String s1=" ";
+	for(int i=s.length()-1;i>=0;i--) {
+		s1=s1+s.charAt(i);
+	}
+	System.out.println(s1);
+}
+}

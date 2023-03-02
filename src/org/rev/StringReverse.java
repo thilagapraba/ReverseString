7b6ab324e5e7a6de6c6a71d300b4c811a1e61f46
thilagapraba <thilagapraba8479@gmail.com>
2023-03-02 21:03:27 +0530

    prab work

--- src/org/rev/StringReverse.java
+++ src/org/rev/StringReverse.java
@@ -9,5 +9,9 @@ public static void main(String[] args) {
 		s1=s1+s.charAt(i);
 	}
 	System.out.println(s1);
+	boolean contains=s.contains("t");String[] args2 = new String[args.length + 1];
+	System.arraycopy(args, 0, args2, 0, args.length);
+	args2[args.length] = s1;
+	System.out.println(contains);
 }
 }
